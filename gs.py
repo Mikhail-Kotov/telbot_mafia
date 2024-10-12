86911fc3fb1105dd91506253b83b2e85553cf973
mk <mike.kotov@gmail.com>
2024-10-13 10:20:24 +1100

    Sunday changes ;) one more

--- gs.py
+++ gs.py
@@ -1,6 +1,6 @@
+import json
 import gspread
 import pandas as pd
-import secret_data as secret
 from oauth2client.service_account import ServiceAccountCredentials
 from pprint import pprint
 
@@ -11,25 +11,27 @@ def user_row(df: pd.DataFrame, t_id: str) -> str:
     return str(df[df[0] == t_id].index[0] + 1)
 
 google_client = gspread.authorize(
-    ServiceAccountCredentials.from_json_keyfile_name("credentials.json",
+    ServiceAccountCredentials.from_json_keyfile_name(".config/gsheet_credentials.json",
                                                      ["https://spreadsheets.google.com/feeds",
-                                                     "https://www.googleapis.com/auth/drive"]))
+                                                      "https://www.googleapis.com/auth/drive"]))
 
-google_sheet = google_client.open_by_url(secret.google_sheets_url)
+secret = json.load(open('.config/telbot_credentials.json', 'r'))
+google_sheet = google_client.open_by_url(secret['google_sheets_url'])
 
 users_sheet = google_sheet.worksheet('users')
+#                A    B          C       D     E             F              G        H        I        J         K       L
 users_headers= ['ID','NICKNAME','TELID','LOC','REGISTERED', 'TELUSERNAME', 'FNAME', 'LNAME', 'PHONE', 'EMAIL',	'FBID',	'STATUS']
 def create_dict(headers):
     return {header: chr(65 + i) for i, header in enumerate(headers)}
 uh = create_dict(users_headers)
-print(uh)
+
+#              A    B           C        D        E             F          G         H        I          J
+cal_headers= ['ID','NICKNAME', 'FNAME', 'LNAME', 'REGISTERED', 'UPDATED', 'COMING', 'PAYED', 'COMMENT', 'MTIME']
+cal_sheet_choosen = None
 
 # cal_sheet = google_sheet.worksheet('MEL 6/10')
-cal_sheet_choosen = None
-cal_headers= ['ID','NICKNAME', 'FNAME', 'LNAME', 'REGISTERED', 'PAYED', 'COMMENT']
-
 # pprint(cal_sheet.col_values(1))
 # df_users = pd.DataFrame(users_sheet.get_all_values())
 # print(df_users)
-# t_id = 't_012345'
+# t_id = 't_394944528'
 # print(user_row(df_users, t_id))
